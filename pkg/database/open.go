429152b5dd5d08c07b685a2f664a18326473ae85
begenov <begenovorazali@icloud.com>
2023-05-26 22:53:04 +0600

    init open database

---
+++ pkg/database/open.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func OpenDB(driver string, dsn string) (*sql.DB, error) {
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
