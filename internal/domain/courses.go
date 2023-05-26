2777cace0d99b4e066643145d5224eaaade040c1
begenov <begenovorazali@icloud.com>
2023-05-26 22:59:49 +0600

    init courses

---
+++ internal/domain/courses.go
@@ -0,0 +1,12 @@
+package domain
+
+import "time"
+
+type Courses struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"-"`
+	UpdatedAt   time.Time `json:"-"`
+	Students    []string  `json:"students"`
+}
