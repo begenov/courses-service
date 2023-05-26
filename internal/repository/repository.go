90d95a503c79e28a2bb7ea73078b79dff368f203
begenov <begenovorazali@icloud.com>
2023-05-26 22:59:23 +0600

    init repository

---
+++ internal/repository/repository.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/begenov/courses-service/internal/domain"
+)
+
+type Courses interface {
+	Create(ctx context.Context, course domain.Courses) error
+	GetByID(ctx context.Context, id int) (domain.Courses, error)
+	Update(ctx context.Context, course domain.Courses) error
+	Delele(ctx context.Context, id int) error
+	GetCoursesByIdStudent(ctx context.Context, studentId string) ([]domain.Courses, error)
+}
+
+type Repository struct {
+	Courses Courses
+}
+
+func NewRepository(db *sql.DB) *Repository {
+	return &Repository{
+		Courses: NewCoursesRepo(db),
+	}
+}
