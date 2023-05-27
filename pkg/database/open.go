78496f3809c0851cd867582c919c44f844be801b
begenov <begenovorazali@icloud.com>
2023-05-27 17:03:31 +0600

    update

--- pkg/database/open.go
+++ pkg/database/open.go
@@ -3,20 +3,18 @@ package database
 import (
 	"context"
 	"database/sql"
-	"log"
 
 	_ "github.com/lib/pq"
 )
 
 func OpenDB(driver string, dsn string) (*sql.DB, error) {
-	log.Println(driver, dsn, "------------------------")
+
 	db, err := sql.Open(driver, dsn)
 	if err != nil {
 		return nil, err
 	}
 
 	if err := db.PingContext(context.Background()); err != nil {
-		log.Println(err, "-----")
 		return nil, err
 	}
 
