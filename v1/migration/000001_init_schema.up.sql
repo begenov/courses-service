59066790b6f07844037356aa99c1f17d7e3e6b7e
begenov <begenovorazali@icloud.com>
2023-06-09 17:29:36 +0600

    update

--- v1/migration/000001_init_schema.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS course (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    students TEXT[],
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-)
